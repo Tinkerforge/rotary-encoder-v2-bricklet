681efb1f39959d8e6c9a47f8aea80f768133f843
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:15:03 +0100

    Add go examples.

---
+++ software/examples/go/example_callback.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/rotary_encoder_v2_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Rotary Encoder Bricklet 2.0.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	re, _ := rotary_encoder_v2_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	re.RegisterCountCallback(func(count int32) {
+		fmt.Printf("Count: %d\n", count)
+	})
+
+	// Set period for count callback to 1s (1000ms) without a threshold.
+	re.SetCountCallbackConfiguration(1000, false, 'x', 0, 0)
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
---
+++ software/examples/go/example_simple.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/rotary_encoder_v2_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Rotary Encoder Bricklet 2.0.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	re, _ := rotary_encoder_v2_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Get current count without reset.
+	count, _ := re.GetCount(false)
+	fmt.Printf("Count: \n", count)
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
