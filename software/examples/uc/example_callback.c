9c96203ac3932c384da0db526b9a8a9a6a144cca
Erik Fleckstein <erik@tinkerforge.com>
2020-08-19 16:53:25 +0200

    Add uc examples.

---
+++ software/examples/uc/example_callback.c
@@ -0,0 +1,40 @@
+
+
+#include "bindings/hal_common.h"
+#include "bindings/bricklet_rotary_encoder_v2.h"
+
+#define UID "XYZ" // Change XYZ to the UID of your Rotary Encoder Bricklet 2.0
+
+void check(int rc, const char* msg);
+
+
+
+// Callback function for count callback
+void count_handler(TF_RotaryEncoderV2 *device, int32_t count, void *user_data) {
+	(void)device; (void)user_data; // avoid unused parameter warning
+
+	tf_hal_printf("Count: %d\n", count);
+}
+
+
+TF_RotaryEncoderV2 re;
+
+void example_setup(TF_HalContext *hal) {
+	// Create device object
+	check(tf_rotary_encoder_v2_create(&re, UID, hal), "create device object");
+
+
+	// Register count callback to function count_handler
+	tf_rotary_encoder_v2_register_count_callback(&re,
+	                                            count_handler,
+	                                            NULL);
+
+	// Set period for count callback to 1s (1000ms) without a threshold
+	tf_rotary_encoder_v2_set_count_callback_configuration(&re, 1000, false, 'x', 0, 0);
+
+}
+
+void example_loop(TF_HalContext *hal) {
+	// Poll for callbacks
+	tf_hal_callback_tick(hal, 0);
+}
---
+++ software/examples/uc/example_simple.c
@@ -0,0 +1,31 @@
+
+
+#include "bindings/hal_common.h"
+#include "bindings/bricklet_rotary_encoder_v2.h"
+
+#define UID "XYZ" // Change XYZ to the UID of your Rotary Encoder Bricklet 2.0
+
+void check(int rc, const char* msg);
+
+
+
+
+TF_RotaryEncoderV2 re;
+
+void example_setup(TF_HalContext *hal) {
+	// Create device object
+	check(tf_rotary_encoder_v2_create(&re, UID, hal), "create device object");
+
+
+	// Get current count without reset
+	int32_t count;
+	check(tf_rotary_encoder_v2_get_count(&re, false, &count), "get count without reset");
+
+	tf_hal_printf("Count: %d\n", count);
+
+}
+
+void example_loop(TF_HalContext *hal) {
+	// Poll for callbacks
+	tf_hal_callback_tick(hal, 0);
+}
