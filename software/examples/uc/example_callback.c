671737c909f6e55cb5f0fa7063fcd35d789bfc8f
Erik Fleckstein <erik@tinkerforge.com>
2022-04-19 16:04:19 +0200

    examples: uc: Rename TF_HalContext to TF_HAL.

--- software/examples/uc/example_callback.c
+++ software/examples/uc/example_callback.c
@@ -9,8 +9,8 @@
 
 void check(int rc, const char* msg);
 
-void example_setup(TF_HalContext *hal);
-void example_loop(TF_HalContext *hal);
+void example_setup(TF_HAL *hal);
+void example_loop(TF_HAL *hal);
 
 
 // Callback function for count callback
@@ -22,7 +22,7 @@ static void count_handler(TF_RotaryEncoderV2 *device, int32_t count, void *user_
 
 static TF_RotaryEncoderV2 re;
 
-void example_setup(TF_HalContext *hal) {
+void example_setup(TF_HAL *hal) {
 	// Create device object
 	check(tf_rotary_encoder_v2_create(&re, UID, hal), "create device object");
 
@@ -35,7 +35,7 @@ void example_setup(TF_HalContext *hal) {
 	tf_rotary_encoder_v2_set_count_callback_configuration(&re, 1000, false, 'x', 0, 0);
 }
 
-void example_loop(TF_HalContext *hal) {
+void example_loop(TF_HAL *hal) {
 	// Poll for callbacks
 	tf_hal_callback_tick(hal, 0);
 }
--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -9,13 +9,13 @@
 
 void check(int rc, const char* msg);
 
-void example_setup(TF_HalContext *hal);
-void example_loop(TF_HalContext *hal);
+void example_setup(TF_HAL *hal);
+void example_loop(TF_HAL *hal);
 
 
 static TF_RotaryEncoderV2 re;
 
-void example_setup(TF_HalContext *hal) {
+void example_setup(TF_HAL *hal) {
 	// Create device object
 	check(tf_rotary_encoder_v2_create(&re, UID, hal), "create device object");
 
@@ -26,7 +26,7 @@ void example_setup(TF_HalContext *hal) {
 	tf_hal_printf("Count: %I32d\n", count);
 }
 
-void example_loop(TF_HalContext *hal) {
+void example_loop(TF_HAL *hal) {
 	// Poll for callbacks
 	tf_hal_callback_tick(hal, 0);
 }
